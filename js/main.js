8225dd5f798a183f619131aab22d492c3207bc07
Carlos Diego Tavares <102318991+diego-tavares@users.noreply.github.com>
2022-10-16 21:41:37 -0300

    adicionado loading

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const diasDaSemana = ["Segunda-Feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira"];
 const button = document.getElementById('button');
-const dia = document.getElementById('dia')
+const dia = document.getElementById('dia');
+const roleta = document.getElementById('roleta')
 
 function gerarDia () {
    randomDay = diasDaSemana[Math.floor(Math.random() * diasDaSemana.length)];
+   roleta.style.display = "flex";
 
    dia.innerText = randomDay;
 }
