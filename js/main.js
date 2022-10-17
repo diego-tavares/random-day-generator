1d72ee1586ef1176d116365003664c1a27ff834e
Carlos Diego Tavares <102318991+diego-tavares@users.noreply.github.com>
2022-10-16 21:18:41 -0300

    Adicionado alteração no texto para o evento click

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-const diasDaSemana = ["Segunda-Feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira"]
-const button = document.getElementById('button')
+const diasDaSemana = ["Segunda-Feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira"];
+const button = document.getElementById('button');
+const dia = document.getElementById('dia')
 
 function gerarDia () {
-   diasDaSemana[Math.floor(Math.random() * diasDaSemana.length)]
+   randomDay = diasDaSemana[Math.floor(Math.random() * diasDaSemana.length)];
+
+   dia.innerText = randomDay;
 }
 
-button.addEventListener('click', gerarDia )
+button.addEventListener('click', gerarDia)
+
